bd83fea43b9f1e64945f0dcb0f344a5a0a0f74f0
Rahul Nair <rahulunair@gmail.com>
2017-01-06 15:04:42 -0600

    Updated the script to display all kinds of urls based on the response

--- check.sh
+++ check.sh
@@ -1,7 +1,45 @@
-echo "---------------------------------"
-for n in $`grep -Erho "(http|https)://[^ ,\">'{}]+$" *.sh | sort | uniq`:
+#!/usr/bin/env bash
+printf "%100s\n"| tr " " -
+# for each result in the regex grep output of URLs
+# curl and see if we are getting a 200/300 status
+no_resp_urls=()
+failed_urls=()
+success_urls=()
+echo  "Verifying if URLs in *.rst and *.md documents are valid, please wait, will take few seconds..."
+for n in $`find . -name "*.rst" -o -name "*.md" | xargs grep -Erho "(http|https)://[^ ,\">'{}]+"  | sort | uniq` :
 do
-	echo -n $n ::
-	curl -Is $n | head -n 1
+	stat=$(curl -Is $n -m 3 | head -n 1)
+	if [ -z "${stat}" ];then
+		no_resp_urls+=($n)
+	fi
+	if [ -n "${stat}" ];then
+		s_code=$stat | cut -d " " -f 2
+		if $s_code > 399;then
+			failed_urls+=($n)
+		else
+			success_urls+=($n)
+		fi
+	fi
 done
-echo "---------------------------------"
+
+printf "%100s\n"| tr " " -
+printf "URLs that responded with 2XX or 3XX (success) ::"
+for s in ${success_urls[@]}:
+do
+	printf "%s\n" "$s"
+done
+
+printf "%100s\n"| tr " " -
+printf "URLs that responded with 4XXs ::"
+for f in ${failed_urls[@]}:
+do
+	printf "%s\n" "$f"
+done
+
+printf "%100s\n"| tr " " -
+printf "URLs that did not return any response ::\n"
+for n in ${no_resp_urls[@]}:
+do
+	printf "%s\n" "$n"
+done
+printf "%100s\n"| tr " " -
